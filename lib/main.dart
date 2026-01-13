8ebee551abb98738f4da7e8e3495f75fd1b5880b
sabbirhossain21t-coder <sabbirhossain21t@gmail.com>
2026-01-13 17:00:17 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'আখো',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'NotoSansBengali', // বাংলা ফন্ট
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
